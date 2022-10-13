c2e60a9763e40cfec444e9d1be8cee5000d9cee7
S Rabi <rabi.sanaye@yahoo.com>
2022-10-13 05:26:32 +0200

    envelope ex

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
-export default function App() {
-    return (
-        <div>
-            <h1>My React App</h1>
-        </div>
-    )
+import React from "react";
+import "./component/style.css";
+export default function App(props) {
+  return (
+    <div className="container">
+      <div className="sender">
+        <h5> {props.senderInfo.name} </h5>
+        <p> {props.senderInfo.streetAdd}</p>
+        <p> {props.senderInfo.country} </p>
+      </div>
+      <div className="stamp">
+        <p>stamp</p>
+      </div>
+      <div className="receiver">
+        <h5> {props.receiverInfo.name} </h5>
+        <p> {props.receiverInfo.streetAdd}</p>
+        <p> {props.receiverInfo.country} </p>
+      </div>
+    </div>
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const sender = {
+  name: "Rabi Sanaye",
+  streetAdd: "123 JG St.",
+  country: "Dubai 22527, UAE",
+};
+const receiver = {
+  name: "Rami Al Sadi",
+  streetAdd: "124 Berlinerstr.",
+  country: "22527 Frankfurt, Germany",
+};
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+  <App senderInfo={sender} receiverInfo={receiver} />,
+  document.getElementById("root")
 );
